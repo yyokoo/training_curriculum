03c6c7269c3a2fe2fe3a0e8b950bfaf81e5a50b2
yyokoo <revaldan1357@gmail.com>
2023-01-26 16:21:29 +0900

    merge前に修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -34,23 +34,9 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-<<<<<<< HEAD
 
-
-      wday_num = Date.today.wday + x
-       
-
-      if wday_num >= 7
-        wday_num = wday_num -7
-      end
-
-      
-      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, :wday => wdays[wday_num]}
-      
-
-=======
       days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
->>>>>>> parent of e8c9631 (Revert "Merge pull request #7 from yyokoo/first_training")
+
       @week_days.push(days)
     end
 
