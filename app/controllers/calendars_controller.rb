a6d74d2a907046a199f9594da2c5c1a87fca79d5
yyokoo <122515959+yyokoo@users.noreply.github.com>
2023-01-26 15:38:00 +0900

    Revert "Second training"

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -34,17 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = Date.today.wday + x
-       
-
-      if wday_num >= 7
-        wday_num = wday_num -7
-      end
-
-      
-      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, :wday => wdays[wday_num]}
-      
+      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
